e924a04aee54502e251cff298bd030b4be5cf7e2
Brandon Mitchell <b.mitch1989@gmail.com>
2016-03-18 00:23:44 -0400

    added stuff

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+var mainController = require('../controllers/main');
 
 var homepageController = function (req, res) {
 	res.render('index', { title: 'Express' });
 };
+
+/* GET home page. */
+router.get('/', mainController.index);
+
+module.exports = router;
