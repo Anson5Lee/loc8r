b437578741de14fcdccee03afcd707ef17190cad
Brandon Mitchell <b.mitch1989@gmail.com>
2016-03-28 05:22:02 -0400

    Added navbar

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
-router.get('/', mainController.index);
+/* GET Locations pages. */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* GET Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
