3d57181574138152400014ac4df9c4dcf7f773b6
Brandon <b.mitch1989@gmail.com>
2016-03-29 04:40:11 -0400

    finished basic hard-coded homepage

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,6 @@
 // GET Home page
 module.exports.homelist = function(req, res) {
-	res.render('index', { title: 'Home' });
+	res.render('locations-list', { title: 'Home' });
 };
 // GET Location Info page
 module.exports.locationInfo = function(req, res) {
