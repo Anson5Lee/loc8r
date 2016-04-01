de6bf2d4b9f977047ccfbf2a3c645caeab5a39be
Brandon Mitchell <b.mitch1989@gmail.com>
2016-03-31 21:55:58 -0400

    moved data from location-info page to controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -9,10 +9,11 @@ module.exports.homelist = function(req, res) {
 			title: 'Loc8r',
 			strapline: "Find places to work with wifi near you!"
 		},
-		sidebar: "Looking for wifi and a seat? Loc8r helps you " +
-			"find places to work when out and about. Perhaps with " +
-			"coffee, cake or a pint? Let Loc8r help you find the " +
-			"place you're looking for.",
+		sidebar: 
+			"Looking for wifi and a seat? Loc8r helps you "
+		  + "find places to work when out and about. Perhaps with "
+		  + "coffee, cake or a pint? Let Loc8r help you find the "
+		  + "place you're looking for.",
 		locations: [{
 			name: 'Starcups',
 			address: '125 High Street, Reading, RG6 1PS',
@@ -37,7 +38,47 @@ module.exports.homelist = function(req, res) {
 };
 // GET Location Info page
 module.exports.locationInfo = function(req, res) {
-	res.render('location-info', { title: 'Location Info' });
+	res.render('location-info', { 
+		title: 'Starcups Info',
+		rating: 3,
+		address: "125 High Street, Reading, RG6 1PS",
+		openingHours: [
+			"Sunday	6AM–9PM",
+			"Monday	5:30AM–9PM",
+			"Tuesday 5:30AM–9PM",
+			"Wednesday 5:30AM–9PM",
+			"Thursday 5:30AM–9PM",
+			"Friday	5:30AM–9PM",
+			"Saturday 6AM–9PM"
+		],
+		facilities: ['Hot Drinks', 'Food', 'Premium Wifi'],
+		locationMap: 'http://maps.googleapis.com/maps/api/staticmap?center=51.455041,-0.9690884&zoom=17&size=400x350&sensor=false&markers=51.455041,-0.9690884&scale=2',
+		addReviewURL: '/location/review/new',
+		custReviews: [{
+			rating: 5,
+			author: "Simon Holmes",
+			timestamp: "16 July 2013",
+			content: "What a great place. I can't say "
+				   + "enough good things about it."
+		},{
+			rating: 3,
+			author: "Charlie Chaplin",
+			timestamp: "16 June 2013",
+			content: 
+				"it was okay.. coffee wasn't great, " 
+			  + "but the wifi was fast."
+		}],
+		description: 
+			"Simon's cafe is on Loc8r because it has " 
+		  + "accessible wifi and space to sit down " 
+		  + "with your laptop and get some work done.",
+		endNote: 
+			"If you've been here and you like it--or " 
+		  + "if you don't-- please leave a review to " 
+		  + "help other people just like you."
+
+
+	});
 };
 // GET Add Review page
 module.exports.addReview = function(req, res) {
