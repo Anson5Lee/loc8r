fbad49c56c27b131e919ddce40890426b72736ff
Brandon Mitchell <b.mitch1989@gmail.com>
2016-04-07 17:06:04 -0400

    Fixed naming typeo 'sendJSONresponse >> sendJsonResponse'

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -8,6 +8,7 @@ var sendJsonResponse = function (res, status, content) {
 
 var theEarth = (function(){
 	var earthRadius = 6371; //km
+
 	var getDistanceFromRads = function(rads) {
 		return parseFloat(rads * earthRadius);
 	};
@@ -20,10 +21,11 @@ var theEarth = (function(){
 	};
 })();
 
+/* GET list of locations */
 module.exports.locationsListByDistance = function (req, res) {
 	var lng = parseFloat(req.query.lng);
 	var lat = parseFloat(req.query.lat);
-	var maxDistance = req.query.maxDistance;
+	var maxDistance = parseFloat(req.query.maxDistance);
 	var point = {
 		type: "Point",
 		coordinates: [lng, lat]
@@ -33,23 +35,45 @@ module.exports.locationsListByDistance = function (req, res) {
 		maxDistance: theEarth.getRadsFromDistance(maxDistance), // sets max distance: 20 km
 		num: 10
 	};
-	Loc.geoNear(point, geoOptions, function (err, results, stats) {
-		var locations = [];
-		results.forEach(function(doc) {
-			locations.push({
-				distance: theEarth.getDistanceFromRads(doc.dis),
-				name: doc.obj.name,
-				address: doc.obj.address,
-				rating: doc.obj.rating,
-				facilites: doc.obj.facilities,
-				_id: doc.obj._id
-			});
+	if (!lng || !lat || !maxDistance) {
+		console.log('locationsListByDistance missing params');
+		sendJSONresponse(res, 404, {
+			"message" : "lng, lat and maxDistance query parameters are all required"
 		});
-		sendJsonResponse(res, 200, locations);
+		return;
+	}
+	Loc.geoNear(point, geoOptions, function(err, results, stats) {
+		var locations;
+		console.log('Geo Results', results);
+		console.log('Geo Stats', stats);
+		if (err) {
+			console.log('geoNear error:', err);
+			sendJsonResponse(res, 404, err);
+		} else {
+			locations = buildLocationList(req, res, results, stats);
+			sendJsonResponse(res, 200, locations);
+		}
 	});
 };
+
+var buildLocationList = function (req, res, results, stats) {
+	var locations = [];
+	results.forEach(function(doc) {
+		locations.push({
+			distance: theEarth.getDistanceFromRads(doc.dis),
+			name: doc.obj.name,
+			address: doc.obj.address,
+			rating: doc.obj.rating,
+			facilites: doc.obj.facilities,
+			_id: doc.obj._id
+		});
+	});
+	return locations;
+};
+
 module.exports.locationsCreate = function (req, res) {
 };
+
 module.exports.locationsReadOne = function (req, res) {
 	if (req.params && req.params.locationid) {
 		Loc
