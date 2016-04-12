e65e4041d80c9fa2e1f73de09fadbb6b49a57e85
Brandon Mitchell <b.mitch1989@gmail.com>
2016-04-12 06:32:47 -0400

    added the request package

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,15 +1,16 @@
+var request = require('request');
 // GET Home page
 module.exports.homelist = function(req, res) {
-	
+
 /*	The second parameter is a data object being sent
 	to the view (the locations-list.jade file).		*/
-	res.render('locations-list', { 
+	res.render('locations-list', {
 		title: 'Loc8r - find a place to work with wifi',
 		pageHeader: {
 			title: 'Loc8r',
 			strapline: "Find places to work with wifi near you!"
 		},
-		sidebar: 
+		sidebar:
 			"Looking for wifi and a seat? Loc8r helps you "
 		  + "find places to work when out and about. Perhaps with "
 		  + "coffee, cake or a pint? Let Loc8r help you find the "
@@ -38,7 +39,7 @@ module.exports.homelist = function(req, res) {
 };
 // GET Location Info page
 module.exports.locationInfo = function(req, res) {
-	res.render('location-info', { 
+	res.render('location-info', {
 		title: 'Starcups Info',
 		address: "125 High Street, Reading, RG6 1PS",
 		rating: 3,
@@ -61,25 +62,25 @@ module.exports.locationInfo = function(req, res) {
 		addReviewURL: '/location/review/new',
 		reviews: [{
 			author: 'Simon Holmes',
-			rating: 5,	
+			rating: 5,
 			createdOn: '16 July 2013',
 			content: "What a great place. I can't say "
 				   + "enough good things about it."
 		},{
 			author: "Charlie Chaplin",
-			rating: 3,	
+			rating: 3,
 			createdOn: "16 June 2013",
-			content: 
-				"it was okay.. coffee wasn't great, " 
+			content:
+				"it was okay.. coffee wasn't great, "
 			  + "but the wifi was fast."
 		}],
-		description: 
-			"Simon's cafe is on Loc8r because it has " 
-		  + "accessible wifi and space to sit down " 
+		description:
+			"Simon's cafe is on Loc8r because it has "
+		  + "accessible wifi and space to sit down "
 		  + "with your laptop and get some work done.",
-		endNote: 
-			"If you've been here and you like it--or " 
-		  + "if you don't-- please leave a review to " 
+		endNote:
+			"If you've been here and you like it--or "
+		  + "if you don't-- please leave a review to "
 		  + "help other people just like you."
 
 
